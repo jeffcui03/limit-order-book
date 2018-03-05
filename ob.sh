522f310ad137efa77a09a1ca8cbeb624599b4bf9
jeffcui03 <jeffcui03@yahoo.com>
2018-03-05 11:44:09 +0800

    update

--- ob.sh
+++ ob.sh
@@ -6,3 +6,46 @@
 
 java -jar target/ob-jar-with-dependencies.jar
 
+
+    if (lastDecay_ == 0)
+    {
+        lastDecay_ = clock_ / decayInterval_ * decayInterval_;
+    }
+    if (clock_ - lastDecay_ < decayInterval_)
+        return;
+
+    lastDecay_ = lastDecay_ + decayInterval_;
+
+    portsig_st_ =0.0;
+    portsig_mt_ =0.0;
+    portsig_lt_ =0.0;
+    double counter =0.0;
+    Instruments *iu = Instruments::instance();
+    Instruments::SymbolInstrumentMapIter it = iu->begin(), eIt = iu->end();
+    for (; it != eIt; ++it)
+    {
+        tradeHistory_[it->second].Book_Sig_Dyn_ *= rho_bk_;
+        tradeHistory_[it->second].Book_Sig_Dyn_lt_ *= rho_bk_lt_;
+
+        double vwap_sig_st = 0;
+        double vwap_sig_mt = 0;
+        double vwap_sig_lt = 0;
+        if(tradeHistory_[it->second].trades_[0].price_.px()>0 && tradeHistory_[it->second].vwap_st_>0)
+            vwap_sig_st = ((tradeHistory_[it->second].trades_[0].price_.px()) - (tradeHistory_[it->second].vwap_st_))/tradeHistory_[it->second].range_st_;
+        if(tradeHistory_[it->second].trades_[0].price_.px()>0 && tradeHistory_[it->second].vwap_mt_>0)
+            vwap_sig_mt = ((tradeHistory_[it->second].trades_[0].price_.px()) - (tradeHistory_[it->second].vwap_mt_))/tradeHistory_[it->second].range_st_;
+        if(tradeHistory_[it->second].trades_[0].price_.px()>0 && tradeHistory_[it->second].vwap_lt_>0)
+            vwap_sig_lt = ((tradeHistory_[it->second].trades_[0].price_.px()) - (tradeHistory_[it->second].vwap_lt_))/tradeHistory_[it->second].range_st_;
+        portsig_st_ += vwap_sig_st;
+        portsig_mt_ += vwap_sig_mt;
+                counter ++;
+    }
+    if(counter >=1)
+    {
+        portsig_st_ /= counter;
+        portsig_mt_ /= counter;
+        portsig_lt_ /= counter;
+    }
+
+
+
